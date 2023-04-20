fd72e883904be94f6489986156de5524ef947cf3
miguel angel lap <miguel.robles@atmosfera.unam.mx>
2023-04-20 02:22:38 -0500

    convierte csv a json, version inicial

--- csv2json.py
+++ csv2json.py
@@ -1,11 +1,26 @@
 from sys import argv
+import csv
 
 filename = argv[1]
-data=[]
+data={}
+names = []
 with open(filename) as pfile:
-    for i,line in enumerate(pfile):
-        if (i!=0):
-            for j,d in enumerate(line.split(',')):
-                data.append
-                if (j!=0):
+    csvreader = csv.reader(pfile, delimiter=',')
+    for i,line in enumerate(csvreader):
+        if (i==0):
+            for j,d in enumerate(line):
+                data[d]=[]
+                names.append(d)
 
+        else:
+            for j,d in enumerate(line):
+                if (j!=0):
+                    if (d==''):
+                        data[names[j]].append(0.0)
+                    else:
+                        data[names[j]].append(float(d))
+                else:
+                    data[names[j]].append(d.replace(' ','T'))
+
+with open(filename.split('.')[0]+'.json', 'w') as pfile:
+    print(data, file=pfile)
